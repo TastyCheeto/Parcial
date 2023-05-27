1f9187b79eba4b81790b5f9a6105d9570fcf9e87
TastyCheeto <e.cabal120@outlook.com>
2023-05-27 08:12:53 -0500

    agregadas pantallas de registro y agregar evento

--- ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
+++ ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.parkingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button next = (Button) findViewById(R.id.loginbutton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+               startActivity(intent);
+            }
+        });
+
+        Button registernext = (Button) findViewById(R.id.signupbutton);
+        registernext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), register_Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
---
+++ ParkingApp/app/src/main/java/com/example/parkingapp/register_Activity.java
@@ -0,0 +1,14 @@
+package com.example.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
